0daabdd096c602864ffb88f1bbc49649b2efc371
YourGitHubUsername <yashduofusion@gmail.com>
2025-08-11 16:18:24 +0530

    commit

--- src/database/models/company.ts
+++ src/database/models/company.ts
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 const CompanySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    number: { type: String, required: true },
-    staringTime: { type: String, required: true },
-    endingTime: { type: String, required: true },
+    name: { type: String },
+    address: { type: String },
+    number: { type: String },
+    staringTime: { type: String, },
+    endingTime: { type: String, },
+    isDeleted: { type: Boolean, default: false },
+}, {
+    timestamps: true, versionKey: false
 })
 
 export const companyModel = mongoose.model('company', CompanySchema);
