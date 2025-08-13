1593bafbf24b213b3c2683de8480ab735278ee75
shakil Gaha <shakilgaha786@gmail.com>
2025-08-13 16:02:24 +0530

    role isBlocked changes

--- src/validation/role.ts
+++ src/validation/role.ts
@@ -7,6 +7,7 @@ export const addRoleSchema = Joi.object().keys({
 export const editRoleSchema = Joi.object().keys({
     roleId: Joi.string().required(),
     name: Joi.string().optional(),
+    isBlocked: Joi.boolean().optional(),
 });
 
 export const deleteRoleSchema = Joi.object().keys({
