9092cf641adfc050c5afb2c9e932e78808d3a8a7
YourGitHubUsername <yashduofusion@gmail.com>
2025-08-18 15:49:45 +0530

    phone number condition changes

--- src/controllers/company/index.ts
+++ src/controllers/company/index.ts
@@ -14,7 +14,7 @@ export const add_company = async (req, res) => {
         let isExist = await getFirstMatch(companyModel, { name: value.name, isDeleted: false }, {}, {});
         if (isExist) return res.status(400).json(new apiResponse(400, responseMessage?.dataAlreadyExist('name'), {}, {}));
 
-        isExist = await getFirstMatch(companyModel, { number: value.number, isDeleted: false }, {}, {});
+        isExist = await getFirstMatch(companyModel, { phoneNumber: value.phoneNumber, isDeleted: false }, {}, {});
         if (isExist) return res.status(400).json(new apiResponse(400, responseMessage?.dataAlreadyExist('number'), {}, {}));
 
         const response = await createData(companyModel, value);
@@ -27,13 +27,8 @@ export const add_company = async (req, res) => {
     }
 }
 
-<<<<<<< HEAD
-export const GetAllUser = async (req, res) => {
-    reqInfo(req);
-=======
 export const edit_company_by_id = async (req, res) => {
     reqInfo(req)
->>>>>>> 1593bafbf24b213b3c2683de8480ab735278ee75
     try {
         const { error, value } = editCompanySchema.validate(req.body)
         if (error) return res.status(501).json(new apiResponse(501, error?.details[0]?.message, {}, {}))
@@ -124,67 +119,7 @@ export const get_company_by_id = async (req, res) => {
         if (!response) return res.status(404).json(new apiResponse(404, responseMessage?.getDataNotFound("Company"), {}, {}));
         return res.status(200).json(new apiResponse(200, responseMessage?.getDataSuccess("Company"), response, {}));
     } catch (error) {
-<<<<<<< HEAD
-        console.error(error);
-        return res.status(500).json(new apiResponse(500, "Internal Server Error", {}, error));
-
-    }
-
-}
-
-// export const updateUser = async (req, res) => {
-//     reqInfo(req);
-//     const body = req.body;
-
-//     try {
-
-//         let isExist = await getFirstMatch(companyModel, { _id: new ObjectId(body.userId), idDeleted: false }, {}, {});
-
-//         if (!isExist) return res.status(404).json(new apiResponse(404, responseMessage?.dataAlreadyExist('company'), {}, {}));
-
-
-
-//         // if (!isExist) return res.status(404).json(new apiResponse(404, responseMessage?.getDataNotFound('company'), {}, {}));
-
-
-//         //     const User = await getFirstMatch(companyModel, { _id: new ObjectId(id), isDeleted: false }, {}, {});
-//         // if (!User) return res.status(404).json(new apiResponse(404, responseMessage?.getDataNotFound('company'), {}, {}));
-
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json(new apiResponse(500, "Internal Server Error", {}, error));
-
-//     }
-
-// }
-
-export const EditUser = async (req, res) => {
-    reqInfo(req);
-    const { id } = req.params;
-    const body = req.body;
-    try {
-        const EditUser = await companyModel.findOneAndEdit()
-    } catch (error) {
-        
-    }
-
-}
-
-export const updateUser = async (req, res) => {
-    reqInfo(req)
-    try {
-        const body = req.body;
-        const { id } = req.body;
-        const updatedUser = await companyModel.findOneAndUpdate({ _id: new ObjectId(id), isDeleted: false }, body, { new: true });
-        if (!updatedUser) {
-            return res.status(404).json(new apiResponse(404, responseMessage.updateDataError("latestUpdate"), {}, {}))
-        }
-        return res.status(200).json(new apiResponse(200, responseMessage.updateDataSuccess("news is updated"), updatedUser, {}))
-    } catch (error) {
-        return res.status(500).json(new apiResponse(500, responseMessage.internalServerError, {}, error))
-=======
         console.log(error);
         return res.status(500).json(new apiResponse(500, responseMessage?.internalServerError, {}, error));
->>>>>>> 1593bafbf24b213b3c2683de8480ab735278ee75
     }
 }
