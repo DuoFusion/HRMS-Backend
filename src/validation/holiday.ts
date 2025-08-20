219e159081705a9a3353e9edc651be4c71f504ff
duofusiondharmik <duofusiondharmik@gmail.com>
2025-08-20 18:06:07 +0530

    user joi add

--- src/validation/holiday.ts
+++ src/validation/holiday.ts
@@ -1,17 +1,17 @@
 import Joi from "joi";
 import { HOLIDAY_TYPE } from "../common";
 
-
-export const addHolidaySchema = Joi.object().keys({
+export const addHolidaySchema = Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
-    date: Joi.string().required(),
+    date: Joi.date().iso().required(),
     type: Joi.string().valid(...Object.values(HOLIDAY_TYPE)).optional(),
     isRecurring: Joi.boolean().required(),
-    createdBy: Joi.string().required(),
-    // updatedBy: Joi.string().required(),
-    // isDeleted: Joi.string().required()
+    createdBy: Joi.string().hex().length(24).optional(),
+    updatedBy: Joi.string().hex().length(24).optional(),
+    isDeleted: Joi.string().optional()
 });
+
 
 export const getHolidaytSchema = Joi.object().keys({
     id: Joi.string().required(),
@@ -29,7 +29,7 @@ export const updateHolidaySchema = Joi.object().keys({
 });
 
 export const deleteHolidaySchema = Joi.object().keys({
-    id : Joi.string().required()
+    id: Joi.string().required()
 })
 
 export const getAllHolidaySchema = Joi.object().keys({
