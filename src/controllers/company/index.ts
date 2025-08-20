a973d0895197f6fab87285f12ae0c5eef4d55efe
duofusiondharmik <duofusiondharmik@gmail.com>
2025-08-20 17:26:03 +0530

    company edit

--- src/controllers/company/index.ts
+++ src/controllers/company/index.ts
@@ -78,6 +78,7 @@ export const get_all_company = async (req, res) => {
         if (search) {
             criteria.$or = [
                 { name: { $regex: search, $options: 'si' } },
+                { ownerName: { $regex: search, $options: 'si' } },
             ];
         }
 
