567d3e39edbfd5089cf1d3287e7e963a930bf416
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2025-08-22 19:34:13 +0530

    log remove

--- src/controllers/company/index.ts
+++ src/controllers/company/index.ts
@@ -90,7 +90,7 @@ export const get_all_company = async (req, res) => {
             options.skip = (parseInt(page) - 1) * parseInt(limit);
             options.limit = parseInt(limit);
         }
-        console.log("criteria => ",criteria)
+        
         const response = await getDataWithSorting(companyModel, criteria, {}, options);
         const totalCount = await countData(companyModel, criteria);
 
