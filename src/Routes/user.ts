d3976933b50e303eac02955ea4cd8e7c15e31b22
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2025-09-01 16:52:04 +0530

    changes API access

--- src/Routes/user.ts
+++ src/Routes/user.ts
@@ -6,7 +6,7 @@ import { ROLES } from '../common';
 const router = Router();
 
 router.post('/add', VALIDATE_ROLE([ROLES.ADMIN]), userController.add_user)
-router.post('/edit', VALIDATE_ROLE([ROLES.ADMIN]), userController.edit_user_by_id);
+router.post('/edit', VALIDATE_ROLE([ROLES.ADMIN, ROLES.HR, ROLES.PROJECT_MANAGER, ROLES.EMPLOYEE]), userController.edit_user_by_id);
 router.delete('/:id', VALIDATE_ROLE([ROLES.ADMIN]), userController.delete_user_by_id);
 router.get('/all', VALIDATE_ROLE([ROLES.ADMIN]), userController.get_all_users);
 router.get('/:id', VALIDATE_ROLE([ROLES.ADMIN, ROLES.HR, ROLES.PROJECT_MANAGER, ROLES.EMPLOYEE]), userController.get_user_by_id);
