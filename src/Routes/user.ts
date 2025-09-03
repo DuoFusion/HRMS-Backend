c9e8b23f93b5170900a4c9d37b941644fb0ae854
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2025-09-03 18:03:54 +0530

    user change

--- src/Routes/user.ts
+++ src/Routes/user.ts
@@ -5,10 +5,10 @@ import { ROLES } from '../common';
 
 const router = Router();
 
-router.post('/add', VALIDATE_ROLE([ROLES.ADMIN]), userController.add_user)
+router.post('/add', VALIDATE_ROLE([ROLES.ADMIN, ROLES.HR]), userController.add_user)
 router.post('/edit', VALIDATE_ROLE([ROLES.ADMIN, ROLES.HR, ROLES.PROJECT_MANAGER, ROLES.EMPLOYEE]), userController.edit_user_by_id);
-router.delete('/:id', VALIDATE_ROLE([ROLES.ADMIN]), userController.delete_user_by_id);
-router.get('/all', VALIDATE_ROLE([ROLES.ADMIN]), userController.get_all_users);
+router.delete('/:id', VALIDATE_ROLE([ROLES.ADMIN, ROLES.HR]), userController.delete_user_by_id);
+router.get('/all', VALIDATE_ROLE([ROLES.ADMIN, ROLES.HR]), userController.get_all_users);
 router.get('/:id', VALIDATE_ROLE([ROLES.ADMIN, ROLES.HR, ROLES.PROJECT_MANAGER, ROLES.EMPLOYEE]), userController.get_user_by_id);
 
 export const userRoutes = router;
