10dc26570bc6ae028b6c79e64267c0f6fbe7ce32
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2025-12-15 14:14:59 +0530

    dburl changes

--- src/database/connection/index.ts
+++ src/database/connection/index.ts
@@ -6,7 +6,7 @@ const dbUrl: any = config.DB_URL;
 const mongooseConnection = express()
 mongoose.set('strictQuery', false)
 mongoose.connect(
-    dbUrl
+    dbUrl, { serverSelectionTimeoutMS: 30000,  socketTimeoutMS: 45000, connectTimeoutMS: 30000 }
 ).then(() => console.log('Database successfully connected')).catch(err => console.log(err));
 
 export { mongooseConnection }
